7fcfb69b9d5aa7d476a0b91d60663153ec8d2ab4
Aman Sharma <mail.aman@protonmail.com>
2023-07-13 01:24:04 +0400

    Revert "added authentication routes along with jwt token creation/verification"
    This reverts commit b9baba94

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,13 @@ import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} fr
 import {Home} from "./pages/Home.jsx";
 import {NewUser} from "./pages/NewUser.jsx";
 import {ExistingUser} from "./pages/ExistingUser.jsx";
-import {Landing} from "./pages/Landing.jsx";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <>
-            <Route path="/" element={<Landing />} />
+            <Route path="/" element={<Home />} />
             <Route path="/Signin" element={<ExistingUser />} />
             <Route path="/Signup" element={<NewUser />} />
-            <Route path="/Home" element={<Home />} />
         </>
     )
 )
